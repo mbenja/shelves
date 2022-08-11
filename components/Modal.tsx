fd148106e16b9fa63719115f7f97d36a53949b2d
Benjamin Streit <benjaminmstreit@gmail.com>
2022-08-10 21:21:32 -0500

    Creating generic Modal component, refactoring NewBookshelfModal

---
+++ components/Modal.tsx
@@ -0,0 +1,58 @@
+import { Fragment, ReactNode } from 'react';
+
+import { Dialog, Transition } from '@headlessui/react';
+
+export default function Modal({
+	title,
+	description,
+	isOpen,
+	onClose,
+	children
+}: {
+	title: string;
+	description?: string;
+	isOpen: boolean;
+	onClose: () => void;
+	children: ReactNode;
+}) {
+	return (
+		<Transition show={isOpen} as={Fragment}>
+			<Dialog className="relative z-50" onClose={onClose}>
+				<Transition.Child
+					as={Fragment}
+					enter="ease-out duration-300"
+					enterFrom="opacity-0"
+					enterTo="opacity-100"
+					leave="ease-in duration-200"
+					leaveFrom="opacity-100"
+					leaveTo="opacity-0"
+				>
+					<div className="bg-black/30 fixed inset-0" />
+				</Transition.Child>
+				<div className="fixed inset-0 flex items-center justify-center">
+					<Transition.Child
+						as={Fragment}
+						enter="ease-out duration-300"
+						enterFrom="opacity-0 scale-95"
+						enterTo="opacity-100 scale-100"
+						leave="ease-in duration-200"
+						leaveFrom="opacity-100 scale-100"
+						leaveTo="opacity-0 scale-95"
+					>
+						<Dialog.Panel className="bg-neutral-100 dark:bg-neutral-600 max-w-md mx-auto p-4 rounded">
+							<Dialog.Title className="font-semibold text-xl">
+								{title}
+							</Dialog.Title>
+							{description && (
+								<Dialog.Description className="mb-4">
+									{description}
+								</Dialog.Description>
+							)}
+							{children}
+						</Dialog.Panel>
+					</Transition.Child>
+				</div>
+			</Dialog>
+		</Transition>
+	);
+}
--- components/NewBookshelfModal.tsx
+++ components/NewBookshelfModal.tsx
@@ -1,8 +1,8 @@
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 
 import Button from './Button';
 import Input from './Input';
-import { Dialog, Transition } from '@headlessui/react';
+import Modal from './Modal';
 
 export default function NewBookshelfModal({
 	isOpen,
@@ -26,61 +26,22 @@ export default function NewBookshelfModal({
 	}
 
 	return (
-		<Transition show={isOpen} as={Fragment}>
-			<Dialog className="relative z-50" onClose={handleClose}>
-				<Transition.Child
-					as={Fragment}
-					enter="ease-out duration-300"
-					enterFrom="opacity-0"
-					enterTo="opacity-100"
-					leave="ease-in duration-200"
-					leaveFrom="opacity-100"
-					leaveTo="opacity-0"
-				>
-					<div className="bg-black/30 fixed inset-0" />
-				</Transition.Child>
-
-				<div className="fixed inset-0 flex items-center justify-center">
-					<Transition.Child
-						as={Fragment}
-						enter="ease-out duration-300"
-						enterFrom="opacity-0 scale-95"
-						enterTo="opacity-100 scale-100"
-						leave="ease-in duration-200"
-						leaveFrom="opacity-100 scale-100"
-						leaveTo="opacity-0 scale-95"
-					>
-						<Dialog.Panel className="bg-neutral-100 dark:bg-neutral-600 max-w-md mx-auto p-4 rounded">
-							<Dialog.Title className="font-semibold text-xl">
-								New Bookshelf
-							</Dialog.Title>
-							<Dialog.Description className="mb-4">
-								Provide a name for your new bookshelf
-							</Dialog.Description>
-
-							<Input
-								placeholder="name"
-								value={name}
-								onChange={(v) => setName(v)}
-							/>
-							<div className="flex gap-2 mt-4">
-								<Button
-									expand
-									text="Cancel"
-									fill="clear"
-									onClick={handleClose}
-								/>
-								<Button
-									expand
-									text="Submit"
-									disabled={name.length === 0}
-									onClick={handleSubmit}
-								/>
-							</div>
-						</Dialog.Panel>
-					</Transition.Child>
-				</div>
-			</Dialog>
-		</Transition>
+		<Modal
+			title="New Bookshelf"
+			description="Provide a name for your new bookshelf"
+			isOpen={isOpen}
+			onClose={handleClose}
+		>
+			<Input placeholder="name" value={name} onChange={(v) => setName(v)} />
+			<div className="flex gap-2 mt-4">
+				<Button expand text="Cancel" fill="clear" onClick={handleClose} />
+				<Button
+					expand
+					text="Submit"
+					disabled={name.length === 0}
+					onClick={handleSubmit}
+				/>
+			</div>
+		</Modal>
 	);
 }
