cb1c6bbab1f10faeb4b3a9f76646336c0e6bb8b5
Benjamin Streit <benjaminmstreit@gmail.com>
2022-08-27 16:48:03 -0500

    Hiding HeaderBar if on auth page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,16 @@
+import { useRouter } from 'next/router';
+
 import { ReactNode } from 'react';
 
+import { ROUTES } from '../lib/constants';
 import HeaderBar from './HeaderBar';
 
 export default function Layout({ children }: { children: ReactNode }) {
+	const router = useRouter();
+
 	return (
 		<div className="flex flex-col h-screen max-w-7xl md:mx-auto md:w-4/5 w-full">
-			<HeaderBar />
+			{router.pathname !== ROUTES.AUTH && <HeaderBar />}
 			<main>{children}</main>
 		</div>
 	);
